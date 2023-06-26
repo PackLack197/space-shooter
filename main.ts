1360cedfbd41aefee5a6269c305f4c7c56d40bc2
PackLack197 <108018568+PackLack197@users.noreply.github.com>
2023-06-26 09:14:42 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+namespace SpriteKind {
+    export const Gas = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(assets.image`projectilepizza`, mySprite, 0, -100)
+    projectile.startEffect(effects.ashes)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprite.destroy(effects.blizzard, 500)
+    otherSprite.destroy()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Gas, function (sprite, otherSprite) {
+    statusbar.value = 100
+    otherSprite.destroy()
+})
+statusbars.onZero(StatusBarKind.Energy, function (status) {
+    game.over(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.disintegrate, 500)
+})
+let myEnemy: Sprite = null
+let myFuel: Sprite = null
+let projectile: Sprite = null
+let statusbar: StatusBarSprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(assets.image`spaceship`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setStayInScreen(true)
+statusbar = statusbars.create(20, 4, StatusBarKind.Energy)
+statusbar.attachToSprite(mySprite, -25, 2)
+game.onUpdateInterval(5000, function () {
+    myFuel = sprites.createProjectileFromSide(assets.image`fuel`, 0, 50)
+    myFuel.x = randint(5, 155)
+    myFuel.setKind(SpriteKind.Gas)
+})
+game.onUpdateInterval(1000, function () {
+    myEnemy = sprites.createProjectileFromSide(assets.image`enemy`, 0, 70)
+    myEnemy.x = randint(5, 155)
+    myEnemy.setKind(SpriteKind.Enemy)
+})
+game.onUpdateInterval(300, function () {
+    statusbar.value += -1
+})
